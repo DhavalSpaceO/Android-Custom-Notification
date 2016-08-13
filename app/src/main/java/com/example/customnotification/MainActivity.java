a79dca8eb68fa190b4f15a6febf7790fc9c39ab2
Dhaval - Laptop <dhavalk.spaceo@gmail.com>
2016-08-13 16:29:45 +0530

    Applied custom layout for notification

--- app/src/main/java/com/example/customnotification/MainActivity.java
+++ app/src/main/java/com/example/customnotification/MainActivity.java
@@ -1,7 +1,26 @@
 package com.example.customnotification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +28,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void onCreateNotification(View view) {
+        CreateCustomNotification(this);
+    }
+
+
+    public void CreateCustomNotification(Context context) {
+
+
+        //Create Intent to launch this Activity on notification click
+        Intent intent = new Intent(context, MainActivity.class);
+
+        // Send data to NotificationView Class
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        // Inflate the notification layout as RemoteViews
+        RemoteViews contentView = new RemoteViews(context.getPackageName(), R.layout.custom_notification);
+
+        int color = Color.BLACK;
+        // Set text on a TextView in the RemoteViews programmatically.
+        contentView.setTextColor(R.id.tvNotificationTitle, ContextCompat.getColor(this, android.R.color.black));
+        contentView.setTextViewText(R.id.tvNotificationTitle, "Android Notification");
+        contentView.setImageViewBitmap(R.id.tvNotificationMessage, textAsBitmap(context, "Apply Custom Notification", color));
+
+
+        Bitmap largeIconBitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                // Set Icon
+                .setLargeIcon(largeIconBitmap)
+                // Sets the ticker text
+                .setTicker(getResources().getString(R.string.app_name))
+                // Sets the small icon for the ticker
+                .setSmallIcon(R.mipmap.ic_launcher)
+                // Dismiss Notification
+                .setAutoCancel(true)
+                // Set PendingIntent into Notification
+                .setContentIntent(pIntent)
+                // Set RemoteViews into Notification
+                .setContent(contentView);
+
+        //Build the notification
+        Notification notification = builder.build();
+
+        // Use the NotificationManager to show the notification
+        NotificationManager notificationmanager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int notificationId = new Random().nextInt(100);
+
+        notificationmanager.notify(notificationId, notification);
+    }
+
+
+    public static Bitmap textAsBitmap(Context context, String text, int textColor) {
+        String fontName = "digital-7";
+        float textSize = 50;
+        Typeface font = Typeface.createFromAsset(context.getAssets(), String.format("%s.ttf", fontName));
+        Paint paint = new Paint();
+        paint.setTextSize(textSize);
+        paint.setTypeface(font);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.5f); // round
+        int height = (int) (baseline + paint.descent() + 20f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
     }
 }
